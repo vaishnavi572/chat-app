19976dd2fc0d4c23b9f7d326c4c4e504499acf8d
vaishnavi572 <gollavaishnavi04@gmail.com>
2026-06-14 16:46:54 +0530

    Rename CSS import to lowercase

--- src/Components/leftside/Leftside.jsx
+++ src/Components/leftside/Leftside.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import "./Leftside.css";
+import "./leftside.css";
 import assets from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
 
