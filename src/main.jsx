d1871c779fb52dccc40cfa35091c120a1fc15645
vaishnavi572 <gollavaishnavi04@gmail.com>
2026-06-14 16:38:30 +0530

    Fix import case for Appcontextprovider component

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Appcontextprovider from './context/appcontext.jsx'
+import Appcontextprovider from './context/Appcontext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
