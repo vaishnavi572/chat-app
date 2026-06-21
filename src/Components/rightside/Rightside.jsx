742b74337158ebee5cd03b20a90ed5f4760af65e
vaishnavi572 <gollavaishnavi04@gmail.com>
2026-06-21 20:07:45 +0530

    Refactor Rightside component for better safety

--- src/Components/rightside/Rightside.jsx
+++ src/Components/rightside/Rightside.jsx
@@ -5,46 +5,77 @@ import { logout } from '../../config/firebase'
 import { AppContext } from '../../context/Appcontext'
 
 function Rightside() {
-  const{chatuser,messages,userdata}=useContext(AppContext)
-  const [msgImages,setmsgImages]=useState()
+  const { chatuser, messages, userdata } = useContext(AppContext)
 
-  useEffect(()=>{
-      let tempvar=[]
-      messages.map((msg)=>{
-        if(msg.image){
-          tempvar.push(msg.image)
-        }
-      })
-      setmsgImages(tempvar)
-  },[messages])
+  // FIX: initialize as empty array
+  const [msgImages, setmsgImages] = useState([])
 
-  return chatuser?(
+  useEffect(() => {
+    let tempvar = []
+
+    // FIX: check messages exists
+    messages?.forEach((msg) => {
+      if (msg.image) {
+        tempvar.push(msg.image)
+      }
+    })
+
+    setmsgImages(tempvar)
+  }, [messages])
+
+  return chatuser ? (
     <div className='rs'>
       <div className="rs-profile">
-        <img src={chatuser.userdata.avatar} alt="" />
-        <h3>{chatuser.userdata.name}{Date.now()-chatuser.userdata.lastSeen<=7000?<img className='dot' src={assets.green_dot} alt=''/>:null}</h3>
-        <p>{chatuser.userdata.bio}</p>
+
+        {/* FIX: optional chaining */}
+        <img src={chatuser?.userdata?.avatar || assets.avatar_icon} alt="" />
+
+        <h3>
+          {chatuser?.userdata?.name}
+          {Date.now() - (chatuser?.userdata?.lastSeen || 0) <= 7000 ? (
+            <img className='dot' src={assets.green_dot} alt='' />
+          ) : null}
+        </h3>
+
+        <p>{chatuser?.userdata?.bio}</p>
       </div>
+
       <hr />
-      <div className="rs-media"><p>Media</p>
-      <div>
-        {msgImages.map((url,index)=>(<img onClick={()=>window.open(url)} key={index} src={url} alt=''/>))}
-        {/* <img src={assets.pic1} alt="" />
-        <img src={assets.pic2} alt="" />
-        <img src={assets.pic3} alt="" />
-        <img src={assets.pic4} alt="" />
-        <img src={assets.pic1} alt="" />
-        <img src={assets.pic2} alt="" /> */}
+
+      <div className="rs-media">
+        <p>Media</p>
+
+        <div>
+          {msgImages.map((url, index) => (
+            <img
+              onClick={() => window.open(url)}
+              key={index}
+              src={url}
+              alt=''
+            />
+          ))}
+        </div>
       </div>
-      </div>
-      <button onClick={()=>{logout()}}>LogOut</button>
+
+      <button
+        onClick={async () => {
+          await logout()
+        }}
+      >
+        LogOut
+      </button>
     </div>
-  ):(
+  ) : (
     <div className='rs'>
-      <button onClick={()=>logout()}>LogOut</button>
+      <button
+        onClick={async () => {
+          await logout()
+        }}
+      >
+        LogOut
+      </button>
     </div>
   )
-
 }
 
 export default Rightside
